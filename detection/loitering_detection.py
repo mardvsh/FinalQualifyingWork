85a429415f6acd2c8f8f621ad5cb9935eb5a8fe9
Marina Mukhina <55767523+mardvsh@users.noreply.github.com>
2023-06-18 18:04:49 +0300

    commit 18.06.2023 hot fix

--- detection/loitering_detection.py
+++ detection/loitering_detection.py
@@ -1,87 +1,154 @@
+import datetime
 from imutils.video import FPS
 import numpy as np
 import imutils
 import time
 import cv2
+from resources.centroidtracker import CentroidTracker
 from resources.SOS import play_notification_sound
 
 prototxt_path = r"C:\Users\marin\PycharmProjects\FinalQualifyingWork\models\MobileNetSSD_deploy.prototxt.txt"
 model_path = r"C:\Users\marin\PycharmProjects\FinalQualifyingWork\models\MobileNetSSD_deploy.caffemodel"
+detector = cv2.dnn.readNetFromCaffe(prototxt=prototxt_path, caffeModel=model_path)
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(prototxt_path, model_path)
+tracker = CentroidTracker(maxDisappeared=80, maxDistance=90)
 
-loitering_time = {}
-loitering_threshold = 70
+def non_max_suppression_fast(boxes, overlapThresh):
+    try:
+        if len(boxes) == 0:
+            return []
+
+        if boxes.dtype.kind == "i":
+            boxes = boxes.astype("float")
+
+        pick = []
+
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+
+        while len(idxs) > 0:
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+
+            overlap = (w * h) / area[idxs[:last]]
+
+            idxs = np.delete(idxs, np.concatenate(([last],
+                                                   np.where(overlap > overlapThresh)[0])))
+
+        return boxes[pick].astype("int")
+    except Exception as e:
+        print("Exception occurred in non_max_suppression : {}".format(e))
 
 
-print("[INFO] starting video stream...")
+def main():
+    #video_source = r"resources/2.mp4"
+    video_source = 0
+    cap = cv2.VideoCapture(video_source)
 
-video_source = 0
-#video_source = r"resources/1_loitering.avi"
+    fps_start_time = datetime.datetime.now()
+    fps = 0
+    total_frames = 0
 
-vs = cv2.VideoCapture(video_source)
-time.sleep(2.0)
-fps = FPS().start()
+    object_id_list = []
+    dtime = dict()
+    dwell_time = dict()
+
+    loitering_threshold = 5  # Пороговое время пребывания (в секундах) для отображения надписи "Loitering!"
+
+    while True:
+        ret, frame = cap.read()
+        frame = imutils.resize(frame, width=600)
+        total_frames = total_frames + 1
+
+        (H, W) = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+
+        detector.setInput(blob)
+        person_detections = detector.forward()
+        rects = []
+        for i in np.arange(0, person_detections.shape[2]):
+            confidence = person_detections[0, 0, i, 2]
+            if confidence > 0.5:
+                idx = int(person_detections[0, 0, i, 1])
+
+                if CLASSES[idx] != "person":
+                    continue
+
+                person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                (startX, startY, endX, endY) = person_box.astype("int")
+                rects.append(person_box)
+
+        boundingboxes = np.array(rects)
+        boundingboxes = boundingboxes.astype(int)
+        rects = non_max_suppression_fast(boundingboxes, 0.3)
+
+        objects = tracker.update(rects)
+        for (objectId, bbox) in objects.items():
+            x1, y1, x2, y2 = bbox
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+
+            if objectId not in object_id_list:
+                object_id_list.append(objectId)
+                dtime[objectId] = datetime.datetime.now()
+                dwell_time[objectId] = 0
+            else:
+                curr_time = datetime.datetime.now()
+                old_time = dtime[objectId]
+                time_diff = curr_time - old_time
+                dtime[objectId] = datetime.datetime.now()
+                sec = time_diff.total_seconds()
+                dwell_time[objectId] += sec
+
+            if dwell_time[objectId] > loitering_threshold:
+                text_loitering = "Loitering!"
+                cv2.putText(frame, text_loitering, (x1, y1 - 25), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+                play_notification_sound()
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+            text = "{}|{}".format(objectId, int(dwell_time[objectId]))
+            cv2.putText(frame, text, (x1, y1 - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        fps_end_time = datetime.datetime.now()
+        time_diff = fps_end_time - fps_start_time
+        if time_diff.seconds == 0:
+            fps = 0.0
+        else:
+            fps = (total_frames / time_diff.seconds)
+
+        fps_text = "FPS: {:.2f}".format(fps)
+
+        #cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
+
+        cv2.imshow("Loitering Detection", frame)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
 
 
-while True:
-    ret, frame = vs.read()
-
-    frame = imutils.resize(frame, width=400)
-
-    (h, w) = frame.shape[:2]
-
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
-    net.setInput(blob)
-    detections = net.forward()
-
-    for person_id in list(loitering_time.keys()):
-        if person_id not in person_ids:
-            del loitering_time[person_id]
-
-    person_ids = []
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
-        if confidence > 0.5:
-            class_id = int(detections[0, 0, i, 1])
-            if CLASSES[class_id] == "person":
-                person_id = int(detections[0, 0, i, 3])
-                person_ids.append(person_id)
-
-                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
-
-                cv2.rectangle(frame, (startX, startY), (endX, endY), COLORS[class_id], 2)
-                label = f"Person {person_id}"
-                cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[class_id], 2)
-
-                if person_id in loitering_time:
-                    loitering_time[person_id] += 1
-                    if loitering_time[person_id] > loitering_threshold:
-                        cv2.putText(frame, "Loitering!", (startX, startY - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                    (0, 0, 255), 2)
-                        play_notification_sound()
-                else:
-                    loitering_time[person_id] = 1
-
-    cv2.imshow("Loitering Detection", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord("q"):
-        break
-
-    fps.update()
-
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-cv2.destroyAllWindows()
-vs.release()
+if __name__ == '__main__':
+    main()
